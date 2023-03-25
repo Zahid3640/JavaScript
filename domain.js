965d9ab7bf4581be68d0053598ae076fcf1a1340
Zahid3640 <zahidnawazpahoor11@gmail.com>
2023-03-25 09:50:32 -0700

    all targetting method

---
+++ domain.js
@@ -0,0 +1,18 @@
+var element;
+element=document.all;
+element=document.body;
+element=document.doctype;
+element=document.head;
+element=document.form;
+element=document.URL;
+element=document.links;
+element=document.anchore
+element=document.images;
+element=document.title;
+element=document.domain;
+element=document.baseURI;
+element=document.getElementById("logo");//targeting by id
+element=document.getElementsByClassName("logo")[1];//targeting by class
+element=document.getElementsByTagName("h1")[1];//targeting by tag name
+element=document.getElementById("logo")
+console.log(element);
