6271371aa453da0ca5a720cfd1ea12f49301bbc3
Zahid3640 <zahidnawazpahoor11@gmail.com>
2023-03-25 12:18:26 -0700

    get mehod

--- getattributes.js
+++ getattributes.js
@@ -2,7 +2,7 @@ var element;
 //element=document.getElementById("logo").innerText;
 //element=document.getElementById("BODY").innerHTML;
 //document.getElementsById("logo").getAttribute("class");
-//document.getElementsById("logo").getAttributeNode("id");  get complete id and value
+//element=document.getElementsById("logo").getAttributeNode("id");  get complete id and value
 //document.getElementById("logo").getAttributeNode("id").value; get value
 document.getElementsById("logo").attributes[1].name ;//and value; display all attributes
 console.log(element);
