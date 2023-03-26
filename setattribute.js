f78f847e1718fbd670c07244b3ae7a0455d2b338
Zahid3640 <zahidnawazpahoor11@gmail.com>
2023-03-26 09:19:13 -0700

    sets attribute

---
+++ setattribute.js
@@ -0,0 +1,6 @@
+var element;
+//element=document.getElementById("logo").innerText="zahid nawaz";
+//document.getElementById("logo").setAttribute("id","zahid nawaz");
+//element=docoment.getElementById("logo").getAttribute("id");
+element=document.getElementById("logo").removeAttribute("style");
+console.log(element); 
