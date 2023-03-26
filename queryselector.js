934db37bc80bf2df96243a0c6043e951fdf55611
Zahid3640 <zahidnawazpahoor11@gmail.com>
2023-03-26 10:04:37 -0700

    Query selector

---
+++ queryselector.js
@@ -0,0 +1,6 @@
+var element;
+//element=document.querySelector("#logo").innerText="<h3>zahidu</h3>";
+//element=document.querySelector(".list");
+//element=document.querySelectorAll(".list");
+element=document.querySelector(".list li")[1].innerText;
+console.log(element);
