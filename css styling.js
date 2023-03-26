8163a37164e51eda2eb33c8a1131a0a7e3cb40a7
Zahid3640 <zahidnawazpahoor11@gmail.com>
2023-03-26 10:55:19 -0700

    css method

---
+++ css styling.js
@@ -0,0 +1,11 @@
+var element;
+//element=document.querySelector("#logo").style.border;
+//document.querySelector("#logo").style.backgroundColor="red"; use style method
+//document.querySelector("#logo").className="abc";
+//element=document.querySelector("#logo").className; use className method
+//document.querySelector("#logo").classList="abc xyz bbb";
+//element=document.querySelector("#logo").classList;
+//document.querySelector("#logo").classList.add("xyx","df");
+//element=document.querySelector("#logo").classList;   //use classlist method
+element=document.querySelector("#logo").classList.remove("ahg");
+document.log(element);
