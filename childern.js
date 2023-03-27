42a58fe1c5a40b022a94939de88602a6031f9c70
Zahid3640 <zahidnawazpahoor11@gmail.com>
2023-03-27 03:36:28 -0700

    CHildren & ChildNode

---
+++ childern.js
@@ -0,0 +1,14 @@
+children target on taggs
+//var a= document.getElementById("n").children;
+//console.log(a);
+//var a= document.getElementById("n").children[3];
+//console.log(a);
+//document.getElementById("n").children[3].style.color=("red");
+//var a= document.getElementById("n").children[3];
+//console.log(a);
+//var a= document.getElementById("n").children[3].innerHTML;
+//console.log(a);
+//use child Node
+//target on ElementInternals,Space and Comment
+var a= document.getElementById("n").childNodes;
+console.log(a);
