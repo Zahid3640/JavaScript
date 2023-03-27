2dcd4f8288ec9683473a22481fc5c959ebb348f8
Zahid3640 <zahidnawazpahoor11@gmail.com>
2023-03-27 01:40:40 -0700

    add event method

---
+++ add Event mehtod.js
@@ -0,0 +1,12 @@
+/*document.getElementById("logo").onclick=abc;
+function abc(){
+    document.getElementById("logo").style.background= "green";
+}*/
+/*document.getElementById("nav").onmouseenter=abc;
+function abc(){
+    document.getElementById("nav").style.background= "green";
+}*/
+document.getElementsById("nav").addEventListener("click",function(){
+    document.getElementsById("nav").style.background= "green";
+
+});
