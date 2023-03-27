01de458128f51d68bd9e398fc7a8f3598dd8f2c5
Zahid3640 <zahidnawazpahoor11@gmail.com>
2023-03-27 03:59:08 -0700

    first&last element child

---
+++ firstliastchild.js
@@ -0,0 +1,12 @@
+// first element child
+//var a=document.getElementById("n").firstElementChild;
+//console.log(a);
+//last element child
+//var a=document.getElementById("n").lastElementChild;
+//console.log(a);
+//firstchild
+var a=document.getElementById("n").firstChild;
+console.log(a);
+//lastchild
+var a=document.getElementById("n").lastChild;
+console.log(a);
