99ab7b73966d84ae351ac7dea19c9e0574b78a4b
Zahid3640 <zahidnawazpahoor11@gmail.com>
2023-04-03 15:33:16 -0700

    Create replace and remove child.js

---
+++ replace and remove child.js
@@ -0,0 +1,15 @@
+// var newtag=document.createElement("li");
+//  //console.log(newtag);
+
+//  var createtextnode=document.createTextNode("this is new text");
+// newtag.appendChild(createtextnode);
+// var target=document.getElementById("list");
+// var oldelement=target.children[1];
+// console.log(oldelement);
+// target.replaceChild(newtag,oldelement)
+
+
+//remove child
+var target=document.getElementById("list");
+var oldelement=target.children[1];
+target.removeChild(oldelement)
