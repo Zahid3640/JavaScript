17b45a190904f5e8435b1b621c2fa84e2ebda4cf
Zahid3640 <zahidnawazpahoor11@gmail.com>
2023-04-03 16:16:24 -0700

    is equel node

---
+++ IsEquel method.js
@@ -0,0 +1,4 @@
+var target1=document.getElementById("list").firstElementChild;
+var target2=document.getElementById("list1").firstElementChild;
+var find=target1.isEqualNode(target2);
+console.log(find);
