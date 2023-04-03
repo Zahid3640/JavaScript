3e9c95aee38e59e13a4fc2474b8851bed83eb56d
Zahid3640 <zahidnawazpahoor11@gmail.com>
2023-04-03 16:16:07 -0700

    has attribute

---
+++ hasattribute.js
@@ -0,0 +1,9 @@
+// var target=document.getElementById("list");
+// var find=target.hasAttribute("class");//return answer true or false
+// console.log(find)
+
+
+// haschildnode
+var target=document.getElementById("list");
+var find=target.hasChildNodes();//return answer true or false
+console.log(find)
