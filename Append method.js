11e0b7081ad1bc599a4fe7318515086e059d5ab8
Zahid3640 <zahidnawazpahoor11@gmail.com>
2023-04-05 12:22:04 -0700

    Update Append method.js

--- Append method.js
+++ Append method.js
@@ -1,43 +1,17 @@
-//var newtag=document.createElement("p");
+var newtag=document.createElement("p");
 // console.log(newtag);
 
- //var createtextnode=document.createTextNode("this is new text");
+ var createtextnode=document.createTextNode("this is new text");
 // console.log(createtextnode);
  
 
 // append at last
- //newtag.appendChild(createtextnode);
+ newtag.appendChild(createtextnode);
 //document.getElementById("test").appendChild(newtag);
 // console.log( newtag);
 
 
-// // insert before
+// insert before
 // var target=document.getElementById("test");
 // target.insertBefore(newtag,target.childNodes[0]);
-
-// insert adjacent
-
-// var newtag=document.createElement("p");
-//  var createtextnode=document.createTextNode("this is new heading");
-//  newtag.appendChild(createtextnode);
-//  var target=document.getElementById("test");
-//  target.insertAdjacentElement("beforebegin",newtag);
-//target.insertAdjacentElement("afterbegin",newtag);
-//target.insertAdjacentElement("beforeend",newtag);
-//target.insertAdjacentElement("afterend",newtag);
-
-//adjecent html
-
-// var target=document.getElementById("test");
-// var newtag="<h2>this is heading</h2>";
-
-// target.insertAdjacentHTML ("afterend",newtag);
-//target.insertAdjacentHTML ("beforeend",newtag);
-
-
-// text adajacent
-var target=document.getElementById("test");
-var newtag="this is heading"
-target.insertAdjacentText("beforeend",newtag);;
-
 
