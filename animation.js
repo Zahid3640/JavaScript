eae265dac8f4535068277208e538b40ebd05fc61
Zahid3640 <zahidnawazpahoor11@gmail.com>
2023-04-05 12:46:33 -0700

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,10 +1,12 @@
+// set interval
+// use to set animation  
 var a=0;
  var id=setInterval(anim,100)
 function anim(){
     a=a+10;
     if(a==200)
     {
-          clearInterval(id);
+          clearInterval(id);     //clearinterval kahtam krta h animation ko
     }
     else{
         var target=document.getElementById("set");
